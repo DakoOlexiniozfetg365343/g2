1065e7ff2cac1b86080fef2e7afe8d41ec93f240
Dako <sashadakopro@gmail.com>
2023-04-08 11:28:30 +0300

    add class student

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ class Person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
+
+class Student(Person):
+    mark = 1
+    def __init__(self,name):
+        super().__init__(name, 15)
+
+
+good_student1 = Student("Sasha")
+good_student2 = Student("Slavik")
+
+print(good_student1.mark)
